ab115d196670089a99e54cddb332a783d1bfc7d2
Tami1118 <chenziyi93326@gmail.com>
2023-08-24 19:50:47 +0800

    :triangular_flag_on_post: Refactor: html & scss
    Refactor: html & scss

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 import '/assets/scss/all.scss';
 import './node_modules/bootstrap/dist/js/bootstrap.min';
+import './node_modules/jquery/dist/jquery.min.js';
+
+import './node_modules/swiper/swiper-bundle.min.css';
+import './node_modules/swiper/swiper-bundle.min.js';
+
+
+
+// validation
+$('.needs-validation').each(function(index){
+  $(this).on('submit', function(e){
+    e.preventDefault();
+    $(this).addClass('was-validated');
+  })
+})
+
+
+
 
 console.log("Hello world!");
