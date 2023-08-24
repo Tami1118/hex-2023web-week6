36c57944584ad6dd25d5ccf8248cdf4940ac53ad
Tami1118 <chenziyi93326@gmail.com>
2023-08-25 00:14:34 +0800

    :triangular_flag_on_post: Refactor: AOS & Swiper update
    Refactor: AOS & Swiper update

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 import '/assets/scss/all.scss';
 import './node_modules/bootstrap/dist/js/bootstrap.min';
 import './node_modules/jquery/dist/jquery.min.js';
+import Swiper from 'swiper/bundle';
+import 'swiper/css';
+import AOS from 'aos';
+import './node_modules/aos/dist/aos.css';
 
-import './node_modules/swiper/swiper-bundle.min.css';
-import './node_modules/swiper/swiper-bundle.min.js';
 
-
-
-// validation
+// jquery - validation
 $('.needs-validation').each(function(index){
   $(this).on('submit', function(e){
     e.preventDefault();
@@ -16,6 +16,32 @@ $('.needs-validation').each(function(index){
 })
 
 
+// swiper
+const swiper = new Swiper(".mySwiper", {
+  allowTouchMove: false,
+  slidesPerView: "auto",
+  loop: true,
+  speed: 20000,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+});
+
+const reverseSwiper = new Swiper(".reverseSwiper", {
+  allowTouchMove: false,
+  slidesPerView: "auto",
+  loop: true,
+  speed: 20000,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+    reverseDirection: true,
+  },
+  centeredSlides: true,
+});
 
 
-console.log("Hello world!");
+// AOS
+AOS.init();
