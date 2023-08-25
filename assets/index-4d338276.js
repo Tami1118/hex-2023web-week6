5b96d25ba5dcc003c7332e08ab65a7bacaea615c
Tami1118 <chenziyi93326@gmail.com>
2023-08-25 08:34:53 +0800

    Updates

--- assets/index-4d338276.js
+++
@@ -1,1 +0,0 @@
-import"./main-175bfc41.js";new Swiper(".mySwiper",{allowTouchMove:!1,slidesPerView:"auto",loop:!0,speed:2e4,autoplay:{delay:0,disableOnInteraction:!1},centeredSlides:!0});new Swiper(".reverseSwiper",{allowTouchMove:!1,slidesPerView:"auto",loop:!0,speed:2e4,autoplay:{delay:0,disableOnInteraction:!1,reverseDirection:!0},centeredSlides:!0});
