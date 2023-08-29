223dae66f7d097a4ea866ef44ea9a08e1a7a0a40
Tami1118 <chenziyi93326@gmail.com>
2023-08-29 13:24:14 +0800

    :triangular_flag_on_post: Refactor: main.js refactor

--- main.js
+++ main.js
@@ -8,12 +8,12 @@ import './node_modules/aos/dist/aos.css';
 
 
 // jquery - validation
-$('.needs-validation').each(function(index){
-  $(this).on('submit', function(e){
+$('.needs-validation').each(function(index) {
+  $(this).on('submit', function(e) {
     e.preventDefault();
     $(this).addClass('was-validated');
-  })
-})
+  });
+});
 
 
 // swiper
